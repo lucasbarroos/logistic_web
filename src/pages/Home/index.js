a413d9c9fe1a24615cf239c594d0345566258a27
Lucas Barros <lbarros.comp@gmail.com>
2020-06-13 01:50:42 -0300

    First commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {
+  Container,
+  Title,
+} from './styles';
+
+const Home = () => (
+  <Container>
+    <Title>Home page</Title>
+  </Container>
+);
+
+export default Home;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    min-heigth: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Title = styled.span`
+    font-size: 26px;
+`;
